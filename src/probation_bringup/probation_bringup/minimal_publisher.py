21c7c88c2fde7dd66fae91dfe228dc8d3966fb5c
myradvd <myra.ofc@gmail.com>
2026-09-04 00:45:05 +0800

    [X Axis Movement]

--- src/probation_bringup/probation_bringup/minimal_publisher.py
+++ src/probation_bringup/probation_bringup/minimal_publisher.py
@@ -1,39 +1,47 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-
-from std_msgs.msg import String
-
+from geometry_msgs.msg import Twist
+import subprocess
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(
+            Twist, 
+            '/mavros/setpoint_velocity/cmd_vel_unstamped', 
+            10
+        )
         timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.timer = self.create_timer(timer_period, self.move_vehicle)
+        self.i = 0.0  # Initialized as a float to prevent type errors
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+    def move_vehicle(self):
+        msg = Twist()
+        msg.linear.x = self.i
+        msg.linear.y = 0.0
+        msg.linear.z = 0.0
+        msg.angular.x = 0.0
+        msg.angular.y = 0.0
+        msg.angular.z = 0.0
+
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        self.get_logger().info(f"Publishing: linear.x = {msg.linear.x:.1f}")
+        self.i += 1.0  
 
 
 def main(args=None):
     rclpy.init(args=args)
-
     minimal_publisher = MinimalPublisher()
 
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(minimal_publisher)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        minimal_publisher.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':

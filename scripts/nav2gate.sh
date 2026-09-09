6020b2763dd702f5bbdc7e7f6486c277300cc81a
myradvd <myra.ofc@gmail.com>
2026-09-10 01:46:07 +0800

    Entryscript for navigation to gate: Old Commit Hash Applied

--- scripts/nav2gate.sh
+++ scripts/nav2gate.sh
@@ -5,20 +5,20 @@ set -e
 
 source ~/probation_ws/install/setup.bash
 
-PACKAGE_NAME="task"
+PACKAGE_NAME="probation_bringup"
+
+
+
+# Give everyhign a few seconds to initialize its services
+sleep 5
+
 
 echo "[entrypoint] Step 1: switching to GUIDED mode..."
-/opt/ros/jazzy/bin/ros2 run "$PACKAGE_NAME" mode_switcher
+ros2 run "$PACKAGE_NAME" set_guide
 
-echo "[entrypoint] Step 2: descending to flare gate depth..."
-/opt/ros/jazzy/bin/ros2 run "$PACKAGE_NAME" depth_controller
 
-#only un=comment if u want to debug...
-#echo "[entrypoint] Step 3: starting main navigation..."
-#/opt/ros/jazzy/bin/ros2 run "$PACKAGE_NAME" nav_debugger
-
-echo "[entrypoint] Step 3: starting main navigation..."
-/opt/ros/jazzy/bin/ros2 run "$PACKAGE_NAME" nav_controller
+echo "[entrypoint] Step 2: starting main navigation..."
+ros2 run "$PACKAGE_NAME" solution_template
 
 
 echo "[entrypoint] Mission sequence finished."
